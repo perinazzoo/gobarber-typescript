6b132719ee14278f5ba8c0c3957977512ce686f4
Gabriel Perinazzo <git@gabrielperinazzo.live>
2020-11-28 18:50:49 -0300

    :sparkles: static serve user avatar

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,13 @@ import routes from './routes';
 
 import './database';
 
+import uploadConfig from './config/upload';
+
 const app = express();
 app.use(express.json());
 
 app.use('/', routes);
+app.use('/files', express.static(uploadConfig.directory));
 
 app.listen(3333, () => {
   console.log('Server running 🎉');
